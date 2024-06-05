2d907e5b7ce91389d95d6cae7c4d0d857689236f
anchal <anchal.arun.mohane@accenture.com>
2024-06-05 13:45:06 +0000

    Day2 file changes added

---
+++ Week1/Day2/test1.js
@@ -0,0 +1,5 @@
+var X = "5";
+var Y;
+Y = X;
+console.log("Type Of Y = ", typeof (Y));
+console.log("Value of Y = ", Y);
---
+++ Week1/Day2/test2.js
@@ -0,0 +1,9 @@
+var X = "Hello";
+console.log("Type OF X = ", typeof (X));
+var Y = "Accenture";
+console.log("Y = ", Y);
+Y = 45;
+console.log("Y = ", Y);
+var names = ["Accenture", "Infosys", "Dell", "Wipro", "Cognizant", "SAP"];
+console.log("First Element = ", names[0]);
+console.log("Fifth Element = ", names[4]);
